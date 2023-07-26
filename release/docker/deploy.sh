35cb4407205cde5fd19e45b288503b0168c28055
Arun George Zachariah <azachariah@nvidia.com>
2023-07-26 10:59:28 -0700

    TAO 5.0 Release - Data Services

--- release/docker/deploy.sh
+++ release/docker/deploy.sh
@@ -75,7 +75,7 @@ if [ $BUILD_DOCKER = "1" ]; then
         echo "Skipping wheel builds ..."
     fi
     
-    docker build --pull -f $NV_TAO_DS_TOP/release/docker/Dockerfile.release -t $registry/$repository:$tag $NO_CACHE --network=host $NV_TAO_DS_TOP/.
+    docker build -f $NV_TAO_DS_TOP/release/docker/Dockerfile.release -t $registry/$repository:$tag $NO_CACHE --network=host $NV_TAO_DS_TOP/.
 
     if [ $PUSH_DOCKER = "1" ]; then
         echo "Pusing docker ..."
